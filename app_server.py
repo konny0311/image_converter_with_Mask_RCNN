4a9230234119ebbc2a101628efeaaf6809918828
Ubuntu <ubuntu@ip-172-31-38-138.ap-northeast-1.compute.internal>
2019-08-20 14:07:05 +0000

    WIP line/gray endpoint

--- app_server.py
+++ app_server.py
@@ -12,9 +12,14 @@ import os
 import sys
 import time
 import base64
+import requests
 import image_processor
 sys.path.append(os.path.join(os.getcwd(), 'mask_rcnn'))
 from mask_rcnn.mrcnn_model import MaskRcnnModel
+
+
+ACCESS_TOKEN = '' # line access token
+
 
 # サーバー起動時にモデルインスタンス作成
 mrcnn_model = MaskRcnnModel()
@@ -115,6 +120,45 @@ def blur_gray():
     res.set_header('Access-Control-Allow-Origin', '*') # cross domain
 
     return res
+
+@post('/splash/line/gray')
+def line_gray():
+    """
+    json = {'image_url': image_url
+            'reply_token': request['replyToken']}
+    ステータスコード200とコンテンツのバイナリデータを返します。
+    how to get an image, see more https://developers.line.biz/ja/reference/messaging-api/#get-content
+    """
+    image_url = request['image_url']
+    headers = {'Authorization': 'Bearer {}'.format(ACCESS_TOKEN)}
+    res = requests.post(image_url, headers=headers)
+    #res.contentでバイナリデータ受け取る
+    f = io.BytesIO()
+    upload.save(f)
+    img = cv2.imdecode(np.frombuffer(f.getbuffer(), np.uint8), 1) #image with color(3 channels)
+    # cv2.imwrite('debug.jpg', img)
+    decoding_fin = time.time()
+    img_decoding_time = decoding_fin - s
+    print('decoding time: ', img_decoding_time)
+    result = mrcnn_model.detect(img)
+    res_img = image_processor.make_image_gray(result, img)
+    # res_img = mrcnn_model.detect(img)
+    name = os.path.splitext(upload.filename)[0]
+    cv2.imwrite('{}_gray.jpg'.format(name), res_img)
+
+    detect_fin = time.time()
+    detecting_time = detect_fin - decoding_fin
+    print('detecting time: ', detecting_time)
+
+    _, res_str = cv2.imencode('.jpg', res_img)
+    res_str = base64.b64encode(res_str)
+
+    res = HTTPResponse(status=200, body=res_str)
+    res.set_header('Content-Type', 'image/jpg')
+    res.set_header('Access-Control-Allow-Origin', '*') # cross domain
+
+    return res
+
 
 #trial of downloader
 @route('/images/<file_path:path>')
