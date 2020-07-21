e120974d47d73fe53e0a0144f16448b08fa79821
konny0311 <konotorie@gmail.com>
2020-07-21 22:27:30 +0900

    不要なエンドポイント削除

--- app/app_server.py
+++ app/app_server.py
@@ -10,7 +10,6 @@ import cv2
 import sys
 import time
 import json
-import boto3
 import base64
 import secrets
 import datetime
@@ -22,110 +21,12 @@ from mask_rcnn.mrcnn_model import MaskRcnnModel
 from google.cloud import storage
 from bottle import route, post, run, template, request, hook, static_file, HTTPResponse
 
-ACCESS_TOKEN = os.getenv('LINE_ACCESS_TOKEN')
-REGION = os.getenv('REGION')
-BUCKET = os.getenv('BUCKET')
-FOLDER = os.getenv('LINE_FOLDER')
-ACCESS = 'public-read'
-
 # サーバー起動時にモデルインスタンス作成
 mrcnn_model = MaskRcnnModel()
 
 @route('/')
 def health():
     return 
-
-@post('/splash/gray')
-def gray():
-    ###
-    #keep human area color and make other area gray
-    ###
-    s = time.time()
-    upload = request.files.get('file_1') #html inputフィールドのname属性を指定
-    f = io.BytesIO()
-    upload.save(f)
-    img = cv2.imdecode(np.frombuffer(f.getbuffer(), np.uint8), 1) #image with color(3 channels)
-    # cv2.imwrite('debug.jpg', img)
-    decoding_fin = time.time()
-    img_decoding_time = decoding_fin - s
-    print('decoding time: ', img_decoding_time)
-    result = mrcnn_model.detect(img)
-    res_img = image_processor.make_image_gray(result, img)
-    name = os.path.splitext(upload.filename)[0]
-    # cv2.imwrite('{}_gray.jpg'.format(name), res_img)
-
-    detect_fin = time.time()
-    detecting_time = detect_fin - decoding_fin
-    print('detecting time: ', detecting_time)
-
-    _, res_str = cv2.imencode('.jpg', res_img)
-    res_str = base64.b64encode(res_str)
-
-    res = HTTPResponse(status=200, body=res_str)
-    res.set_header('Content-Type', 'image/jpg')
-    res.set_header('Access-Control-Allow-Origin', '*') # cross domain
-
-    return res
-
-@post('/splash/blur')
-def blur():
-    ###
-    #keep human area focused and make other area blur
-    ###
-    s = time.time()
-    upload = request.files.get('file_1') #html inputフィールドのname属性を指定
-    f = io.BytesIO()
-    upload.save(f)
-    img = cv2.imdecode(np.frombuffer(f.getbuffer(), np.uint8), 1) #image with color(3 channels)
-    decoding_fin = time.time()
-    img_decoding_time = decoding_fin - s
-    print('decoding time: ', img_decoding_time)
-    result = mrcnn_model.detect(img)
-    res_img = image_processor.make_image_blur(result, img)
-    cv2.imwrite('result.jpg', res_img)
-
-    detect_fin = time.time()
-    detecting_time = detect_fin - decoding_fin
-    print('detecting time: ', detecting_time)
-
-    _, res_str = cv2.imencode('.jpg', res_img)
-    res_str = base64.b64encode(res_str)
-
-    res = HTTPResponse(status=200, body=res_str)
-    res.set_header('Content-Type', 'image/jpg')
-    res.set_header('Access-Control-Allow-Origin', '*') # cross domain
-
-    return res
-
-@post('/splash/blur_gray')
-def blur_gray():
-    ###
-    #keep human area focused and make other area blur
-    ###
-    s = time.time()
-    upload = request.files.get('file_1') #html inputフィールドのname属性を指定
-    f = io.BytesIO()
-    upload.save(f)
-    img = cv2.imdecode(np.frombuffer(f.getbuffer(), np.uint8), 1) #image with color(3 channels)
-    decoding_fin = time.time()
-    img_decoding_time = decoding_fin - s
-    print('decoding time: ', img_decoding_time)
-    result = mrcnn_model.detect(img)
-    res_img = image_processor.make_image_blur_gray(result, img)
-    cv2.imwrite('result.jpg', res_img)
-
-    detect_fin = time.time()
-    detecting_time = detect_fin - decoding_fin
-    print('detecting time: ', detecting_time)
-
-    _, res_str = cv2.imencode('.jpg', res_img)
-    res_str = base64.b64encode(res_str)
-
-    res = HTTPResponse(status=200, body=res_str)
-    res.set_header('Content-Type', 'image/jpg')
-    res.set_header('Access-Control-Allow-Origin', '*') # cross domain
-
-    return res
 
 @post('/splash/line')
 def line():
